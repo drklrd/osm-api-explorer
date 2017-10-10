83ace4b72bd538498c5d5da05cd7b66ee21de80b
drklrd <srvbhattarai@gmail.com>
2017-10-10 21:17:00 +0545

    add leaderboard API

--- controllers/index.js
+++ controllers/index.js
@@ -5,6 +5,8 @@ catch(e){
 	console.log('Config file not found !');
 }
 
+var request = require('request');
+
 module.exports = function(router) {
 	router.get('/config',function(req,res){
 		if(process.env.oauth_consumer_key && process.env.oauth_secret && process.env.auto){
@@ -26,7 +28,6 @@ module.exports = function(router) {
 			})
 		}else{
 			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-			var request = require('request');
 			request(`${process.env.apiURL}/${req.params.user}`, function (error, response, body) {
 			  if(error) res.json({
 			  	success : 0,
@@ -40,4 +41,28 @@ module.exports = function(router) {
 		
 
 	});
+
+
+	router.get('/api/v1/leaderboard/:hashtag',function(req,res){
+
+		if(!process.env.leaderboardURL){
+			res.json({
+				success : 0
+			})
+		}else{
+			process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+			request(`${process.env.leaderboardURL}/${req.params.hashtag}/users`, function (error, response, body) {
+			  if(error) res.json({
+			  	success : 0,
+			  	message : error
+			  })
+			  body = JSON.parse(body);
+			  body.success = 1;
+			  res.json(body);
+			});
+
+
+		}
+
+	})
 }
